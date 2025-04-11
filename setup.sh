6812ec939618d2396b0ea0093bc044bfc9c5d743
stu2005 <konstu8@gmail.com>
2025-04-11 15:43:09 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -9,15 +9,3 @@ cp ./samples/epgstation/config.yml.template ./epgstation/config.yml
 cp ./samples/epgstation/operatorLogConfig.sample.yml ./epgstation/operatorLogConfig.yml
 cp ./samples/epgstation/epgUpdaterLogConfig.sample.yml ./epgstation/epgUpdaterLogConfig.yml
 cp ./samples/epgstation/serviceLogConfig.sample.yml ./epgstation/serviceLogConfig.yml
-
-docker compose -f./mirakurun.docker-compose.yaml run --rm -eSETUP=true mirakurun
-docker compose -f./scan.docker-compose.yaml run --rm tvchannels-scan
-docker compose -f./scan.docker-compose.yaml run --rm isdb-scanner
-docker compose -f./mirakurun.docker-compose.yaml run -d --rm mirakurun
-URL="http://localhost:40772/api/status"
-until curl --silent --fail "$URL" > /dev/null; do
-    sleep 2
-done
-curl -X GET "http://localhost:40772/api/channels/scan?type=GR"
-curl -X GET "http://localhost:40772/api/channels/scan?type=BS"
-docker compose -f./mirakurun.docker-compose.yaml down -v
